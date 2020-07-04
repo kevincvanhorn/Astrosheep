8c9227a9af602329b505264d836ff16ebb3663dc
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2020-07-04 18:22:26 -0500

    Base env

---
+++ Source/Astrosheep/AstrosheepGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AstrosheepGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTROSHEEP_API AAstrosheepGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
